PixelExperience-Devices/device_xiaomi_raphael

raphael: parts: Add DiracSound backend
Special thanks to vasishath for his base work on mi3

Change-Id: Id81e60a18dc34c991b82a5a2685c9041222a6ea7

parts/src/org/lineageos/settings/dirac/DiracUtils.java
@@ -17,10 +17,53 @@
 package org.lineageos.settings.dirac;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
 
 public final class DiracUtils {
 
+    protected static DiracSound mDiracSound;
+    private static boolean mInitialized;
+    private static Context mContext;
+
+    public static void initialize(Context context) {
+        if (!mInitialized) {
+            mInitialized = true;
+            mContext = context;
+            mDiracSound = new DiracSound(0, 0);
+            mDiracSound.setMusic(mDiracSound.getMusic());
+            mDiracSound.setHeadsetType(mDiracSound.getHeadsetType());
+            setLevel(getLevel());
+        }
+    }
+
+    protected static void setMusic(boolean enable) {
+        mDiracSound.setMusic(enable ? 1 : 0);
+    }
+
     protected static boolean isDiracEnabled() {
-        return false;
+        return mDiracSound.getMusic() == 1;
+    }
+
+    protected static void setLevel(String preset) {
+        String[] level = preset.split("\\s*,\\s*");
+
+        for (int band = 0; band <= level.length - 1; band++) {
+            mDiracSound.setLevel(band, Float.valueOf(level[band]));
+        }
+    }
+
+    protected static String getLevel() {
+        String selected = "";
+        for (int band = 0; band <= 6; band++) {
+            int temp = (int) mDiracSound.getLevel(band);
+            selected += String.valueOf(temp);
+            if (band != 6) selected += ",";
+        }
+        return selected;
+    }
+
+    protected static void setHeadsetType(int paramInt) {
+         mDiracSound.setHeadsetType(paramInt);
     }
 }

hsx02/device_spes

parts/src/org/lineageos/settings/dirac/DiracUtils.java
@@ -16,9 +16,52 @@
 
 package org.lineageos.settings.dirac;
 
+import android.content.Context;
+
 public final class DiracUtils {
 
+    protected static DiracSound mDiracSound;
+    private static boolean mInitialized;
+    private static Context mContext;
+
+    public static void initialize(Context context) {
+        if (!mInitialized) {
+            mInitialized = true;
+            mContext = context;
+            mDiracSound = new DiracSound(0, 0);
+            mDiracSound.setMusic(mDiracSound.getMusic());
+            mDiracSound.setHeadsetType(mDiracSound.getHeadsetType());
+            setLevel(getLevel());
+        }
+    }
+
+    protected static void setMusic(boolean enable) {
+        mDiracSound.setMusic(enable ? 1 : 0);
+    }
+
     protected static boolean isDiracEnabled() {
-        return false;
+        return mDiracSound.getMusic() == 1;
+    }
+
+    protected static String getLevel() {
+        String selected = "";
+        for (int band = 0; band <= 6; band++) {
+            int temp = (int) mDiracSound.getLevel(band);
+            selected += String.valueOf(temp);
+            if (band != 6) selected += ",";
+        }
+        return selected;
+    }
+
+    protected static void setLevel(String preset) {
+        String[] level = preset.split("\\s*,\\s*");
+
+        for (int band = 0; band <= level.length - 1; band++) {
+            mDiracSound.setLevel(band, Float.valueOf(level[band]));
+        }
+    }
+
+    protected static void setHeadsetType(int paramInt) {
+        mDiracSound.setHeadsetType(paramInt);
     }
 }